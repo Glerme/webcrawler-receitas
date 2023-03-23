7d64ec46bee2a7b3f32d70168a2ab029f589cbe5
Guilherme Felipe <gui@MacBook-Air-de-Guilherme.local>
2023-03-22 21:43:03 -0300

    feat: Adicionado get de urls

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
-import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
+import sys
 import time
 
-url = "https://www.tudoreceitas.com/"
-response = requests.get(url)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
+cmd_ctrl = Keys.COMMAND if sys.platform == 'darwin' else Keys.CONTROL
+
+url = "https://www.tudoreceitas.com/"
 # Inicializa o navegador
 driver = webdriver.Chrome()
+
+ac = ActionChains(driver)
 
 # Abre a página desejada
 driver.get(url)
 driver.maximize_window()
-time.sleep(5)
-
-# Obtém o conteúdo HTML da página
-html = driver.page_source
-
-
-ac = webdriver.ActionChains(driver)
-
 time.sleep(2)
 
-
 driver.find_element(By.ID, 'q').send_keys('sobremesas', webdriver.Keys.ENTER)
+time.sleep(2)
+
+recipesTitles = driver.find_elements(
+    By.CLASS_NAME, 'titulo')
+
+linkForRecipes = []
+
+for recipe in recipesTitles:
+    recipleLink = recipe.get_attribute('href')
+
+    if (recipleLink != None):
+        linkForRecipes.append(recipleLink)
 
 
-time.sleep(5)
+for recipeUrl in linkForRecipes:
+    newDriver = webdriver.Chrome()
+
+    newDriver.get(recipeUrl)
+    elemento_ingredientes = newDriver.find_element(
+        By.CSS_SELECTOR, "div.ingredientes")
+
+    texto_ingredientes = elemento_ingredientes.text
+
+    lista_ingredientes = texto_ingredientes.split("\n")
+
+    print(lista_ingredientes)
