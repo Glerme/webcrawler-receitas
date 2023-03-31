304aba6b29045bd1ceee8164a51ee1160148f79d
Guilherme <guiggff@gmail.com>
2023-03-31 13:54:48 -0300

    fix: Adicionado validação se nao existir a div

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 
 class ReceitasScraper:
@@ -26,52 +27,90 @@ class ReceitasScraper:
         # Abre a página desejada
         self.driver.get(self.url)
         self.driver.maximize_window()
-        time.sleep(2)
+        time.sleep(5)
 
         print('Entrando na tela...')
 
+        # Pesquisar por sobremesas salgadas
         self.driver.find_element(By.ID, 'q').send_keys(
-            'sobremesas', webdriver.Keys.ENTER)
-        time.sleep(2)
+            'sobremesas salgadas', webdriver.Keys.ENTER)
+        time.sleep(5)
 
-        recipes_titles = self.driver.find_elements(By.CLASS_NAME, 'titulo')
+        salgadas_titles = self.driver.find_elements(By.CLASS_NAME, 'titulo')
 
-        link_for_recipes = []
+        salgadas_links = []
 
-        print('Buscando...')
+        print('Buscando sobremesas salgadas...')
 
-        for recipe in recipes_titles:
+        for recipe in salgadas_titles:
             recipe_link = recipe.get_attribute('href')
 
             if (recipe_link is not None):
-                print('Pegando os links das tags a', recipe_link)
-                link_for_recipes.append(recipe_link)
+                print('Pegando os links das sobremesas salgadas', recipe_link)
+                salgadas_links.append(recipe_link)
 
-        for recipe_url in link_for_recipes:
+            if len(salgadas_links) >= 10:
+                break
+
+        # Pesquisar por sobremesas doces
+        self.driver.find_element(By.ID, 'q').clear()
+        self.driver.find_element(By.ID, 'q').send_keys(
+            'sobremesas doces', webdriver.Keys.ENTER)
+        time.sleep(2)
+
+        doces_titles = self.driver.find_elements(By.CLASS_NAME, 'titulo')
+
+        doces_links = []
+
+        print('Buscando sobremesas doces...')
+
+        for recipe in doces_titles:
+            recipe_link = recipe.get_attribute('href')
+
+            if (recipe_link is not None):
+                print('Pegando os links das sobremesas doces', recipe_link)
+                doces_links.append(recipe_link)
+
+            if len(doces_links) >= 10:
+                break
+
+        all_links = salgadas_links + doces_links
+
+        for recipe_url in all_links:
             chrome_options = Options()
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+            chrome_options.add_argument("--disable-javascript")
 
             new_driver = webdriver.Chrome(options=chrome_options)
             print('Buscando os ingredientes na pagina')
 
             new_driver.get(recipe_url)
-            elemento_ingredientes = new_driver.find_element(
-                By.CSS_SELECTOR, "div.ingredientes")
+            new_driver.maximize_window()
+            time.sleep(5)
 
-            texto_ingredientes = elemento_ingredientes.text
+            try:
+                elemento_ingredientes = new_driver.find_element(
+                    By.CSS_SELECTOR, "div.ingredientes")
 
-            lista_ingredientes = texto_ingredientes.split("\n")
+                texto_ingredientes = elemento_ingredientes.text
 
-            nome_arquivo = new_driver.title
-            caminho_arquivo = os.path.join(self.nome_pasta, nome_arquivo)
+                lista_ingredientes = texto_ingredientes.split("\n")
 
-            with open(caminho_arquivo, mode='w', newline='') as file:
-                print('Escrevendo no .txt')
-                for ingrediente in lista_ingredientes:
-                    file.write(ingrediente + '\n')
+                nome_arquivo = new_driver.title
+                caminho_arquivo = os.path.join(self.nome_pasta, nome_arquivo)
+
+                with open(caminho_arquivo, mode='w', newline='') as file:
+                    for ingrediente in lista_ingredientes:
+                        print('Escrevendo no .txt', ingrediente)
+                        file.write(ingrediente + '\n')
+
+            except NoSuchElementException:
+                print('Não foi possível encontrar a div de ingredientes')
+                continue
 
             new_driver.quit()
 
